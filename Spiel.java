a58e49694878d3ea502e215aac829c473b096f8e
Albert <albertheinrichmontag@gmail.com>
2025-01-17 14:22:39 +0100

    Konstruktor für Spieler

--- Spiel.java
+++ Spiel.java
@@ -1,3 +1,5 @@
 public class Spiel{
+
+    private 
     
 }
--- Spieler.java
+++ Spieler.java
@@ -1,3 +1,16 @@
 public class Spieler {
 
+    private String Name;
+    private int Punkte;
+    private int Wette;
+    
+
+    public Spieler (String pName, int pPunkte, int pWette){
+
+        this.Name = pName;
+        this.Punkte = pPunkte;
+        this.Wette = pWette;
+        
+    }
+
 }
