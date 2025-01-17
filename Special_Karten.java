bf068a5d56f49996641a8d123f451b49f092a9bb
Maxim <maxim.helmer.2006@gmail.com>
2025-01-17 15:17:10 +0100

    lol

---
+++ Special_Karten.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+public class Karten{
+    private String farbe;
+    private String wert;
+
+
+
+
+    public Karten(String pfarbe, String pWert){
+
+        this.farbe = pfarbe;
+        this.wert = pWert;
+
+    }
+
+
+    
+}
+
+public class Special_Karten extends Karten 
