834448014d8e37e4ab95954a54208f928c2f769c
Albert <albertheinrichmontag@gmail.com>
2025-01-17 15:17:02 +0100

    Liste hinzugefügt

--- Spieler.java
+++ Spieler.java
@@ -1,16 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Spieler {
 
     private String Name;
     private int Punkte;
     private int Wette;
     
+    
 
     public Spieler (String pName, int pPunkte, int pWette){
-
+        
+        List<String> Karten_Liste = new ArrayList<>();
         this.Name = pName;
         this.Punkte = pPunkte;
         this.Wette = pWette;
         
     }
 
-}
+    
+    } 
+
