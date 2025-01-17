77a56c33e5edc1ef2bc9b0854f043b1037d26952
Sebastian <ss2863651@gmail.com>
2025-01-17 15:28:11 +0100

    comments

--- Karten.java
+++ Karten.java
@@ -15,6 +15,15 @@ public class Karten{
         this.farbe = pfarbe;
         this.wert = pWert;
 
+
+        //Aktionskarten
+        //  >Joker
+        //      >Spieler sagt beliebige Farbe an, Karte wird zur höchsten Karte dieser Farbe
+        //      >Joker kann auch Trumpf werden
+        //      >bei 2 Joker in einem Stich, 2. Joker höher als 1.
+        //      >bei Eröffnung mit Joker muss die angesagte Farbe bedient werden
+        //  >+5/-5
+        //      >
     }
 
 
--- Spiel.java
+++ Spiel.java
@@ -20,5 +20,43 @@ public class Spiel{
             System.out.println(farbe);
             
         }
+        
+        //Spiel hat 10 Runden
+        //Erste Runde 10 Karten, zweite Runde 9 Karten, dritte Runde 8 Karten,...
+
+        //Erster Ausspieler ist random (Bei folgenden Runden weiter nach links verschoben)
+        //Weiteres Ausspielen vom Spieler der den letzten Stich gemacht hat
+
+        //2 Kartenstapel
+        //  >Talon (Ziehstapel)
+        //  >Ablagestapel (Jeder Spieler einen)
+        //Nach dem Austeilen(automatisch), erste Karte vom Talon seperat ablegen (diese farbe ist Trumpf)
+        //  >wenn es eine Aktionskarte ist, gilt die erste gespielte Karte als Trupffarbe
+
+        //Spieler geben ihre Stichwette ab
+        //  >0 Stiche sind auch valide
+        //Ansagen werden notiert/angezeigt
+
+        //Spielrichtung ist im Uhrzeigersinn
+
+        //Es gilt Farbzwang, nur bei nicht besitzen der Farbe andere Karte gestattet
+        //Man kann abwerchen, solange der Farbzwang nicht verletzt wird
+        //Kein Trumpfzwang, 
+        //  >solange man noch andere Karten außer Trumpf besitzt, muss auch nicht mit Trumpf gestochen werden
+
+        //Den Stich gewinnt die höchste Karte in der aktuellen Trumpffarbe
+        //  >Liegt kein Trumpf im Stich, gewinnt die höchste Karte in der zuerst gespielten Farbe
+        //Gewonne Stiche kommen auf den Ablagestapel des Siegers
+
+        //Bei Ende der Runde Punkteabrechnung
+        //  >Richtige Wette +10 Punkte
+        //  >Falsche Wette -5 Punkte
+        //  >Jeder Stich +1 Punkt
+        //  >Aktionskarte in Ablagestapel (+5 Punkte/-5 Punkte)
+
+        //Alle Karten werden erneut gemischt
+        //Jeder Spieler erhält eine Karte weniger als in der Vorrunde
+
+
     }
 }
