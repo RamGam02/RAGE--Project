018e26fc3565801b910f1917da0554fc5bff884b
Sebastian <ss2863651@gmail.com>
2025-02-10 14:23:53 +0100

    Pseudocode erweitert

--- Special_Karten.java
+++ Special_Karten.java
@@ -2,7 +2,7 @@
 public class Karten{
 
 
-public class Karten {    private String farbe;
+    private String farbe;
     private String wert;
 
 
@@ -13,7 +13,6 @@ public class Karten {    private String farbe;
         this.farbe = pfarbe;
         this.wert = pWert;
 
-<<<<<<< HEAD
     }
 
 
@@ -33,10 +32,18 @@ public class Special_Karten {
         //      >bei 2 Joker in einem Stich, 2. Joker höher als 1.
         //      >bei Eröffnung mit Joker muss die angesagte Farbe bedient werden
         //  >+5/-5
-        //      >
+        //      >Wer den Stich gewinnt bekommt die Punkte angerechnet
+        //  >Trumpfwechsel
+        //      >Karte vom Nachziehstapel wird gezogen
+        //      >erste Farbkarte neuer Trumpf
+        //      >Trumpffareb kann auch gleich bleiben
+        //  >Kein Trumpf
+        //      >für aktuellen Stich gibt es dann keinen Trumpf
+        //      >wenn danach Trumpfwechsel gelegt wird gilt dann der neue Trumpf
 
     }
-}
+
+    
 public class Special_Karten extends Karten {
  
     public Special_Karten(String pFarbe, String aktion) {
