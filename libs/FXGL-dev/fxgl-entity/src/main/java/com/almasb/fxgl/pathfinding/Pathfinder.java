c54297efce54621cef8642d35b960b155f7a0c4f
Sebastian <ss2863651@gmail.com>
2025-02-14 15:18:07 +0100

    Adding libs

---
+++ libs/FXGL-dev/fxgl-entity/src/main/java/com/almasb/fxgl/pathfinding/Pathfinder.java
@@ -0,0 +1,47 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.pathfinding;
+
+import com.almasb.fxgl.core.collection.grid.Cell;
+import com.almasb.fxgl.core.collection.grid.NeighborDirection;
+
+import java.util.List;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public interface Pathfinder<T extends Cell> {
+
+    /**
+     * Empty list is returned if no path exists.
+     *
+     * @return a list of cells from source (excl.) to target (incl.)
+     */
+    List<T> findPath(int sourceX, int sourceY, int targetX, int targetY);
+
+    /**
+     * Empty list is returned if no path exists.
+     *
+     * @return a list of cells from source (excl.) to target (incl.)
+     */
+    List<T> findPath(int sourceX, int sourceY, int targetX, int targetY, NeighborDirection neighborDirection);
+
+    /**
+     * Empty list is returned if no path exists.
+     *
+     * @return a list of cells from source (excl.) to target (incl.) while ignoring busyCells
+     */
+    List<T> findPath(int sourceX, int sourceY, int targetX, int targetY, List<T> busyCells);
+
+    /**
+     * Empty list is returned if no path exists.
+     *
+     * @return a list of cells from source (excl.) to target (incl.) while ignoring busyCells
+     */
+    List<T> findPath(int sourceX, int sourceY, int targetX, int targetY, NeighborDirection neighborDirection, List<T> busyCells);
+
+}
